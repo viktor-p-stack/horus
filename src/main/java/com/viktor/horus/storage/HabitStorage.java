5f5951f55893d9ae2ceb3271e7630e449a1cfdc9
Your Name <you@example.com>
2026-01-07 14:48:57 +0100

    Entfernen von Systemdateien.

--- src/main/java/com/viktor/horus/storage/HabitStorage.java
+++ src/main/java/com/viktor/horus/storage/HabitStorage.java
@@ -1,5 +1,8 @@
 package main.java.com.viktor.horus.storage;
 
+import main.java.com.viktor.horus.model.Habit;
+
 public class HabitStorage {
+    // Diese Klasse wird für die Speicherung und Verwaltung der Habits verwendet.
     
 }
